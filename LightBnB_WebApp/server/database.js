051bf0113461efba98106a6dee3748c397328cfb
davemgj84 <davemgj@gmail.com>
2020-09-17 21:21:37 +0000

    allowed getAllProperties function to access lightbnb database using appropriate queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -103,11 +103,56 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  const queryParams = [];
+
+  let queryString = `SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city ILIKE $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    queryString += queryParams.length ? ` AND` : `WHERE`;
+    queryParams.push(options.owner_id);
+    queryString += ` owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    const dollars = options.minimum_price_per_night * 100;
+    queryString += queryParams.length ? ` AND` : `WHERE`;
+    queryParams.push(dollars);
+    queryString += ` cost_per_night >= $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    const dollars = options.maximum_price_per_night * 100;
+    queryString += queryParams.length ? ` AND` : `WHERE`;
+    queryParams.push(dollars);
+    queryString += ` cost_per_night <= $${queryParams.length}`;
+  }
+
+  queryString += `
+  GROUP BY properties.id`;
+  
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  return pool.query(queryString, queryParams)
     .then(res => res.rows);
+
 };
 exports.getAllProperties = getAllProperties;
 
