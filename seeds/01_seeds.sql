9ec49893a40ca09e4bf3a98ce4a4a572c758bbcb
davemgj84 <davemgj@gmail.com>
2020-09-17 00:26:13 +0000

    fixed some errors in the property_reviews

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO users (name, email, password) VALUES ('David Jardine', 'djardine@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 INSERT INTO users (name, email, password) VALUES ('MC Escher', 'escher@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (name, email, password) VALUES ('Pablo Picasso', 'pablo@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Vincent Van-Gogh', 'pablo@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 INSERT INTO users (name, email, password) VALUES ('Claude Monet', 'monet@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 -- properties table data:
@@ -14,7 +14,7 @@ INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover
 VALUES (2, 'Villa des Escher', 'description', 'https://leds-c4.com/sites/default/files/media/images/imatge1_1.jpg', 'https://leds-c4.com/sites/default/files/media/images/imatge1_1.jpg', 1800, 6, 6, 10, 'Netherlands', '123 Awesome Street', 'Amsterdam', 'North Holland', '123 345');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
-VALUES (3, 'Villa des Picasso', 'description', 'https://odis.homeaway.com/odis/listing/900bb556-7d1c-44d4-b104-216ea1dae1a9.c10.jpg', 'https://odis.homeaway.com/odis/listing/900bb556-7d1c-44d4-b104-216ea1dae1a9.c10.jpg', 1900, 6, 5, 11, 'Spain', '123 Rad Street', 'Madrid', '	Community of Madrid', '123 355');
+VALUES (3, 'Villa des Van-Gogh', 'description', 'https://odis.homeaway.com/odis/listing/900bb556-7d1c-44d4-b104-216ea1dae1a9.c10.jpg', 'https://odis.homeaway.com/odis/listing/900bb556-7d1c-44d4-b104-216ea1dae1a9.c10.jpg', 1900, 6, 5, 11, 'Spain', '123 Rad Street', 'Madrid', '	Community of Madrid', '123 355');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
 VALUES (4, 'Villa des Monet', 'description', 'https://www.eland-dreamvillas.com/website/diafora/files/1448/villas-chania-villa-aerial_2.jpg', 'https://www.eland-dreamvillas.com/website/diafora/files/1448/villas-chania-villa-aerial_2.jpg', 1750, 4, 4, 8, 'France', '123 Cool Street', 'Nice', 'Southern Alps', '123 168');
@@ -29,6 +29,6 @@ INSERT INTO reservations (start_date, end_date, property_id, guest_id) VALUES ('
 -- property_reviews table data:
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (1, 2, 1, 10, 'Fantastic stay, great ambiance, had trouble using the stairs..' );
-INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (2, 3, 2, 8, 'Beautiful night sky on the patio beside the cypress trees' );
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (2, 3, 2, 4, 'Beautiful night sky on the patio beside the cypress trees, but found a human ear under the sink!?' );
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (3, 4, 3, 9, 'Great garden!' );
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (4, 1, 4, 10, 'Fantastic Villa!' );
